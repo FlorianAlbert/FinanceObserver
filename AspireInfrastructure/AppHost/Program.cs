45b3eeb5aefc3455bdabb8d074a787d51b9f3868
FlorianAlbert <61190722+FlorianAlbert@users.noreply.github.com>
2025-11-19 21:17:32 +0100

    Introduce database resource, improve dependencies inside the AppHost, replace connection string with resource name

--- AspireInfrastructure/AppHost/Program.cs
+++ AspireInfrastructure/AppHost/Program.cs
@@ -2,9 +2,10 @@ using Aspire.Hosting.MailDev;
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-IResourceBuilder<PostgresServerResource> postgresResource = 
-    builder.AddPostgres("postgres")
-           .WithPgAdmin();
+IResourceBuilder<PostgresServerResource> postgresResource = builder.AddPostgres("postgres")
+    .WithPgAdmin();
+
+IResourceBuilder<PostgresDatabaseResource> database = postgresResource.AddDatabase("finance-observer-db");
 
 IResourceBuilder<MailDevResource> mailDevResource = builder.AddMailDev("maildev")
     .ExcludeFromManifest();
@@ -13,12 +14,14 @@ builder.AddProject<Projects.Startup>("startup")
     .WithEnvironment("FINANCE_OBSERVER_FROM_EMAIL_ADDRESS", "no-reply@finance-observer.com")
     .WithEnvironment("FINANCE_OBSERVER_FROM_EMAIL_NAME", "Finance Observer")
     .WithEnvironment("FINANCE_OBSERVER_DB_PROVIDER", "Npgsql")
-    .WithEnvironment("FINANCE_OBSERVER_DB_CONNECTIONSTRING", "postgres")
+    .WithEnvironment("FINANCE_OBSERVER_DB_CONNECTIONNAME", "finance-observer-db")
     .WithEnvironment("FINANCE_OBSERVER_HASHING_ITERATIONS", "123456")
     .WithEnvironment("FINANCE_OBSERVER_HASHING_HASH_SIZE", "64")
     .WithEnvironment("FINANCE_OBSERVER_HASHING_SALT_SIZE", "16")
     .WithEnvironment("FINANCE_OBSERVER_EXPIRED_REGISTRATION_DELETION_EXECUTION_PERIOD", "60")
-    .WithReference(postgresResource)
-    .WithReference(mailDevResource);
+    .WithReference(database)
+    .WaitFor(database)
+    .WithReference(mailDevResource)
+    .WaitFor(mailDevResource);
 
 builder.Build().Run();
